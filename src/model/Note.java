3662725539114a410d4e7009d93ca72c5233d405
Nipun <nipdeept@gmail.com>
2026-09-18 00:32:20 +0530

    Added setter for the title and content

--- src/model/Note.java
+++ src/model/Note.java
@@ -14,8 +14,16 @@ public class Note {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getContent() {
         return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 
     public int getId() {
