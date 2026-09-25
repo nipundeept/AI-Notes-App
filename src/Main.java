0230271742b623f676588e8d4f4f7fcd15bf390b
Nipun <nipdeept@gmail.com>
2026-09-26 01:40:53 +0530

    Latest changes

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,12 @@
 import model.Note;
+import repository.NoteRepository;
 import service.NoteService;
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
+        NoteRepository repository = new NoteRepository();
+        NoteService service = new NoteService(repository);
+        service.displayAllNotes();
 
-        NoteService service = new NoteService();
-        service.addNote(1, "DBMS", "Learning queries");
-        service.addNote(2, "Java", "Implementing a project in Java");
-        service.addNote(3, "Music", "I like Daft Punk music");
-        for (Note note : service.getAllNotes()) {
-            System.out.println("Note ID : " + note.getId());
-            System.out.println("Title : " + note.getTitle());
-            System.out.println("Content : " + note.getContent());
-        }
-        service.updateNote(3, "Music", "I like Michael Jackson music");
-        System.out.println("After updating : ");
-        for (Note note : service.getAllNotes()) {
-            System.out.println("Note ID : " + note.getId());
-            System.out.println("Title : " + note.getTitle());
-            System.out.println("Content : " + note.getContent());
-        }
-        service.deleteNote(2);
-        System.out.println("After deletion: ");
-        for (Note note : service.getAllNotes()) {
-            System.out.println("Note ID : " + note.getId());
-            System.out.println("Title : " + note.getTitle());
-            System.out.println("Content : " + note.getContent());
-        }
     }
 }
