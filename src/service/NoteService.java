f511f67ffe815cb0a8c0d0c9fd47b667b9789a82
Nipun <nipdeept@gmail.com>
2026-09-26 01:39:18 +0530

    Refactor note service to use repository

--- src/service/NoteService.java
+++ src/service/NoteService.java
@@ -1,55 +1,45 @@
 package service;
 
 import model.Note;
+
+import repository.NoteRepository;
+
 import java.util.ArrayList;
+import java.util.List;
+
 
 public class NoteService {
-    private ArrayList<Note> storage;
-    public NoteService() {
-        storage = new ArrayList<>();
+    private NoteRepository repository;
+    public NoteService(NoteRepository repository) {
+        this.repository = repository;
     }
     public void addNote(int id, String title, String content) {
         Note note = new Note(id, title, content);
-        storage.add(note);
+        repository.save(note);
     }
     //simple get method
-    public ArrayList<Note> getAllNotes() {
+    public List<Note> getAllNotes() {
         //return storage; //if user accesses this, our original storage will be compromised if he does .clear(), instead we return a copy of the storage
-        return new ArrayList<>(storage);
+        return repository.getAllNotes();
     }
     public Note getNoteById(int inputId) {
-        //search for the id
-        for (Note note : storage) {
-            if (note.getId() == inputId) {
-                return note;
-            }
+        return repository.getNoteById(inputId);
+    }
+
+    public void displayAllNotes() {
+        List<Note> noteList = getAllNotes();
+        int notesLength = noteList.size();
+        for (Note note : noteList) {
+            System.out.println("ID : " +note.getId() + " " + "Title : " + note.getTitle());
+            System.out.println(note.getContent());
+            System.out.println();
         }
-        return null;
     }
     public boolean deleteNote(int id) {
-        /*for (Note note : storage) {
-            if (note.getId() == id) {
-                storage.remove(note); //ConcurrentModificationException
-                return true;
-            }
-        }
-        return false;*/
-        //do not repeat the logic when you can reuse an existing method
-        Note current = getNoteById(id);
-        if (current != null) {
-            storage.remove(current);
-            return true;
-        }
-        return false;
+        return repository.deleteNoteById(id);
     }
 
     public boolean updateNote(int id, String newTitle, String newContent) {
-        Note note = getNoteById(id);
-        if (note != null) {
-            note.setTitle(newTitle);
-            note.setContent(newContent);
-            return true;
-        }
-        return false;
+       return repository.updateNote(id, newTitle, newContent);
     }
 }
